0d77bbf452c74e666e39fe7b45e192cb8378cdef
Yemao Luo <leafysn@qq.com>
2022-02-02 15:41:56 +0800

    Add files via upload

--- UpdateTurnelUrl.py
+++ UpdateTurnelUrl.py
@@ -6,11 +6,11 @@ import pymysql.cursors
 if __name__ == '__main__':
     tunnel = 'null'
     connect = pymysql.Connect(
-        host='',
+        host='114.132.44.105',
         port=3306,
         user='root',
-        passwd='',
-        db='',
+        passwd='!Zyy38298989',
+        db='tunnelurl',
         charset='utf8'
     )
 
@@ -20,6 +20,9 @@ if __name__ == '__main__':
         response = requests.get('http://localhost:4040')
         html = response.text
         url = re.findall('[a-zA-z]+://[^\s]*', html)
+        if tunnel == '':
+            print('为探测到隧道 请检查隧道是否正常开启')
+            continue
         if tunnel == url[0][0:25]:
             print('url未改变 url为：' + tunnel)
             continue
