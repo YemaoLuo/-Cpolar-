b19d4806e26b06116dbb4c33d91c14ce7b72bf79
Yemao Luo <leafysn@qq.com>
2022-02-07 11:38:56 +0800

    修复不能及时更新url的bug

--- StartTurnel.py
+++ StartTurnel.py
@@ -1,5 +1,14 @@
+import subprocess
 import os
+import time
 
-command = "cpolar http 8080"
-print("cpolar隧道开启完成")
-os.system(command)
+port = input('开启隧道: ')
+
+while(True):
+  command = "cpolar http " + port
+  print('隧道开启完成')
+  run = subprocess.Popen(command)
+  time.sleep(1800)
+  run.kill()
+  os.system('cls')
+  print('隧道重启中')
--- UpdateTurnelUrl.py
+++ UpdateTurnelUrl.py
@@ -7,6 +7,7 @@ import os
 
 if __name__ == '__main__':
     tunnel = 'null'
+    count = 1
     rec_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     connect = pymysql.Connect(
         host='',
@@ -24,21 +25,25 @@ if __name__ == '__main__':
             url = re.findall('[a-zA-z]+://[^\s]*', html)
             if url[0][4] == 's':
                 continue
+            while(True):
+                if url[0][count- 1] == 'i' and url[0][count] == 'o':
+                    url = url[0][0:count + 1]
+                    break
+                count = count + 1
         except Exception:
             print('隧道异常 请检查隧道状态')
             continue
         try:
-            if tunnel == url[0][0:25]:
+            if tunnel == url:
+                os.system('cls')
                 print('url未改变 url为：' + tunnel)
                 print('最近更新：' + rec_time)
                 print('系统休眠中')
-                time.sleep(30)
-                os.system('cls')
                 continue
         except Exception:
             print("爬取url失败 重试中")
         try:
-            tunnel = url[0][0:25]
+            tunnel = url
             cursor = connect.cursor()
             sql = "UPDATE url set tunnel = '%s' where id = 1"
             cursor.execute(sql % tunnel)
@@ -47,9 +52,10 @@ if __name__ == '__main__':
                 print("数据库数据更新成功")
                 rec_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             cursor.close()
+            os.system('cls')
             print('当前隧道url为：' + tunnel)
             print('系统休眠中')
-            time.sleep(30)
-            os.system('cls')
         except Exception:
+            os.system('cls')
             print("数据库更新失败 重试中")
+            tunnel = 'null'
